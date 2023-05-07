65ee34194aad53985cbbfe802440875968b4d913
metinbarangokdemir <125825997+metinbarangokdemir@users.noreply.github.com>
2023-05-07 23:33:38 +0300

    summary
    A simple data processing application made in C#.

---
+++ Ornek1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Ornek1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtadSoyad = new System.Windows.Forms.TextBox();
+            this.dtkayitTarihi = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtgsmNo = new System.Windows.Forms.TextBox();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.cmbxId = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kayit Tarihi";
+            // 
+            // txtadSoyad
+            // 
+            this.txtadSoyad.Location = new System.Drawing.Point(224, 113);
+            this.txtadSoyad.Name = "txtadSoyad";
+            this.txtadSoyad.Size = new System.Drawing.Size(165, 20);
+            this.txtadSoyad.TabIndex = 1;
+            // 
+            // dtkayitTarihi
+            // 
+            this.dtkayitTarihi.Location = new System.Drawing.Point(224, 87);
+            this.dtkayitTarihi.Name = "dtkayitTarihi";
+            this.dtkayitTarihi.Size = new System.Drawing.Size(165, 20);
+            this.dtkayitTarihi.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ad Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Gsm No";
+            // 
+            // txtgsmNo
+            // 
+            this.txtgsmNo.Location = new System.Drawing.Point(224, 139);
+            this.txtgsmNo.Name = "txtgsmNo";
+            this.txtgsmNo.Size = new System.Drawing.Size(165, 20);
+            this.txtgsmNo.TabIndex = 1;
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.Location = new System.Drawing.Point(224, 165);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnkaydet.TabIndex = 3;
+            this.btnkaydet.Text = "Kaydet";
+            this.btnkaydet.UseVisualStyleBackColor = true;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // cmbxId
+            // 
+            this.cmbxId.FormattingEnabled = true;
+            this.cmbxId.Location = new System.Drawing.Point(224, 60);
+            this.cmbxId.Name = "cmbxId";
+            this.cmbxId.Size = new System.Drawing.Size(165, 21);
+            this.cmbxId.TabIndex = 4;
+            this.cmbxId.SelectedIndexChanged += new System.EventHandler(this.cmbxId_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Id";
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(314, 165);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(413, 165);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 6;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 331);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.cmbxId);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.dtkayitTarihi);
+            this.Controls.Add(this.txtgsmNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtadSoyad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtadSoyad;
+        private System.Windows.Forms.DateTimePicker dtkayitTarihi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtgsmNo;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.ComboBox cmbxId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+    }
+}
+
---
+++ Ornek1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ornek1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OkulEntities ef = new OkulEntities();
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            ogrenciBilgileri yeniOgrenci = new ogrenciBilgileri();
+            yeniOgrenci.adSoyad = txtadSoyad.Text;
+            yeniOgrenci.kayitTarihi = dtkayitTarihi.Value;
+            yeniOgrenci.gsmNo = txtgsmNo.Text;
+
+            ef.ogrenciBilgileri.Add(yeniOgrenci);
+            ef.SaveChanges();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var item in ef.ogrenciBilgileri)
+            {
+                cmbxId.Items.Add(item.Id);
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(cmbxId.Text);
+            var silinecek = ef.ogrenciBilgileri.Where(x => x.Id == id).FirstOrDefault();
+            ef.ogrenciBilgileri.Remove(silinecek);
+            ef.SaveChanges();
+        }
+
+        private void cmbxId_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int id = int.Parse(cmbxId.Text);
+            var goruntulenecek = ef.ogrenciBilgileri.Where(x => x.Id == id).ToList();
+
+            foreach (var item in goruntulenecek)
+            {
+                dtkayitTarihi.Value =Convert.ToDateTime(item.kayitTarihi).Date;
+                txtadSoyad.Text = item.adSoyad;
+                txtgsmNo.Text = item.gsmNo;
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(cmbxId.Text);
+            var update = ef.ogrenciBilgileri.Where(x => x.Id == id).FirstOrDefault();
+
+            update.adSoyad = txtadSoyad.Text;
+            update.gsmNo = txtgsmNo.Text;
+            update.kayitTarihi = Convert.ToDateTime(dtkayitTarihi.Value).Date;
+
+            ef.SaveChanges();
+
+        }
+    }
+}
